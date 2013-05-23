8337fc96d71a0d80344d40deab1245ed4c0e675e
Garrett Moore <gmoore10@fullsail.edu>
2013-05-23 19:44:46 -0400

    Implemented StoreData and Bronze Adjustments

--- Week 3/gold/js/main.js
+++ Week 3/gold/js/main.js
@@ -28,8 +28,34 @@ var getData = function(){
 
 };
 
-var storeData = function(data){
-	
+var storeData = function (data) {
+    var key;
+    if (!key) {
+        var id = Math.floor(Math.random() * 100000001);
+    } else {
+        id = key;
+    }
+    var item = {};
+    item.firstName = ["First Name", data[0].value];
+    item.lastName = ["Last Name", data[1].value];
+    item.toDoName = ["To-Do Name", data[2].value];
+    item.dtDue = ["Due Date", data[4].value];
+    item.assignedTo = ["Assigned To", data[3].value];
+    item.priority = ["Priority", data[5].value];
+    item.sendEmail = ["Send Email to Task Receiver?", data[7].value];
+    item.content = ["Content", data[6].value];
+
+    //Save data into Local Storage: Use Stringify to convert our object to a string.
+    localStorage.setItem(id, JSON.stringify(item));
+    //alert("To-Do Saved!");
+
+    //Reset the Form
+    var myForm = $('#addToDoForm');
+    myForm[0].reset();
+    //Go Home
+    $.mobile.changePage($("#home"));
+
+
 }; 
 
 var	deleteItem = function (){
